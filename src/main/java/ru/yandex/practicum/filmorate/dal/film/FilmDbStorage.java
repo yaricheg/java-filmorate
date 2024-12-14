38ad4aee8a002f0dfcceb57afe039ceebed0de92
Эренджен Бамбушев <Erendzhen.Bambushev@gmail.com>
2024-12-14 15:35:38 +0300

    FilmDbStorage. Поправил фичу с поиском, и добавлением лайка

--- src/main/java/ru/yandex/practicum/filmorate/dal/film/FilmDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/dal/film/FilmDbStorage.java
@@ -153,9 +153,15 @@ public class FilmDbStorage extends BaseRepository<Film> implements FilmStorage {
 
     @Override
     public void addLike(Integer filmId, Integer userId) {
+        final String existsQuery="SELECT COUNT(*) FROM likes WHERE film_id = ? AND user_id = ?";
         final String insertQuery = "INSERT INTO likes (film_id, user_id) values (?, ?)";
         final String increaseRateQuery = "UPDATE films SET rate = rate + 1 WHERE id = ?";
-        jdbc.update(insertQuery, filmId, userId);
+
+        Integer exists= jdbc.queryForObject(existsQuery, Integer.class, filmId, userId);
+        if (exists == 0) {
+            jdbc.update(insertQuery, filmId, userId);
+        }
+
         jdbc.update(increaseRateQuery, filmId);
         addEvent(userId, "ADD", filmId);
     }
@@ -260,15 +266,16 @@ public class FilmDbStorage extends BaseRepository<Film> implements FilmStorage {
         boolean searchByTitle = by.contains("title");
         boolean searchByDirector = by.contains("director");
         List<String> params = new ArrayList<>();
+        query = query.toLowerCase();
 
         params.add("%" + query + "%");
         if (searchByTitle && searchByDirector) {
-            conditions = "WHERE f.name LIKE ? OR d.name LIKE ?\n";
+            conditions = "WHERE LOWER(f.name) LIKE ? OR d.name LIKE ?\n";
             params.add("%" + query + "%");
         } else if (searchByTitle) {
-            conditions = "WHERE f.name LIKE ?\n";
+            conditions = "WHERE LOWER(f.name) LIKE ?\n";
         } else if (searchByDirector) {
-            conditions = "WHERE d.name LIKE ?\n";
+            conditions = "WHERE LOWER(d.name) LIKE ?\n";
         } else {
             throw new ValidationException("Параметр \"by\" некорректен");
         }
@@ -292,8 +299,8 @@ public class FilmDbStorage extends BaseRepository<Film> implements FilmStorage {
                         GROUP BY f.id, m.name
                         ORDER BY likes_count DESC;
                         """;
-
-        return findMany(findByQuery, params.toArray());
+        Collection<Film> films = findMany(findByQuery, params.toArray());
+        return films;
     }
 
 
