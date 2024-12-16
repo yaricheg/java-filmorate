31b6d07afe43580857333ad51b68ec301227ed50
Gnaizel <Gnaizel@mail.ru>
2024-12-16 22:44:37 +0300

    hot fix

--- src/main/java/ru/yandex/practicum/filmorate/mappers/EventRowMapper.java
+++ src/main/java/ru/yandex/practicum/filmorate/mappers/EventRowMapper.java
@@ -2,6 +2,8 @@ package ru.yandex.practicum.filmorate.mappers;
 
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
+import ru.yandex.practicum.filmorate.enums.DbOperation;
+import ru.yandex.practicum.filmorate.enums.EventType;
 import ru.yandex.practicum.filmorate.model.Event;
 
 import java.sql.ResultSet;
@@ -9,14 +11,15 @@ import java.sql.SQLException;
 
 @Component
 public class EventRowMapper implements RowMapper<Event> {
+
     @Override
     public Event mapRow(ResultSet rs, int rowNum) throws SQLException {
 
         return Event.builder()
                 .timestamp(rs.getLong("timestamp"))
                 .userId(rs.getInt("user_id"))
-                .eventType(rs.getString("event_type"))
-                .operation(rs.getString("operation"))
+                .eventType(EventType.valueOf(rs.getString("event_type")))
+                .operation(DbOperation.valueOf(rs.getString("operation")))
                 .eventId(rs.getLong("event_id"))
                 .entityId(rs.getInt("entity_id"))
                 .build();
